fca5a574e92b54e9c727a030852058fd09019811
Mahtab-termo <mahtab.hamd4809@gmail.com>
2022-11-09 11:31:19 -0800

    First commit

---
+++ controllers/auth.js
@@ -0,0 +1,22 @@
+exports.getLogin=(req,res,next)=>{
+    const isLoggedIn=req
+    .get('Cookie')
+    .split('=')[1];
+     res.render('auth/login',{
+        path:'/login',
+        pageTitle:'Login',
+        isAuthenticated:isLoggedIn
+    });
+};
+
+exports.postLogin=(req,res,next)=>{
+    req.session.isLoggedIn=true;
+    res.redirect('/');
+};
+
+exports.postLogout=(req,res,next)=>{
+    req.session.destroy(err=>{
+        console.log(err);
+        res.redirect('/');
+    })
+}
